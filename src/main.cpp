914e3eddbf5ce18e7850b4aac8c154eb59d67c10
dieiev.dmitry <dieiev.dima@gmail.com>
2018-07-29 16:58:52 +0300

    added options parsing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <algorithm>
 #include <cstring>
+#include <sstream>
+#include <unistd.h>
 
 #include "vigenere.hpp"
 
@@ -14,19 +16,64 @@ using std::string;
 
 using namespace vigenere;
 
+enum class COMMAND {
+	ENCODE,
+	DECODE,
+};
+
 int main(int argc, char **argv)
 {
-	string     raw_buf;
-	string		file_name;
+	char c;
+	COMMAND cmd;
+	string raw_buf, in_key;
+	const char *filename;
 
-	if (argc == 2) {
-		file_name.assign(argv[1]);
-	} else {
-		cerr << "Incorrect number of arguments. Terminate." << endl;
+	if (argc < 2) {
+		static std::stringstream ss;
+		ss << "Usage: " << argv[0] << " [options] file" << endl << 
+		"Options:" << endl <<
+		"\t -e encode" << endl <<
+		"\t -d decode" << endl <<
+		"\t -k key (relevant for encode)" << endl;
+
+		cout << ss.str();
+		return 0;
+	}
+
+	while ((c = getopt (argc, argv, "e:d:k:")) != -1)
+		switch (c) {
+		case 'e':
+			cmd = COMMAND::ENCODE;
+			filename = optarg;
+			cout << "ENCODE " << filename << endl; 
+			break;
+		case 'd':
+			cmd = COMMAND::DECODE;
+			filename = optarg;
+			cout << "DECODE " << filename << endl;
+			break;
+		case 'k':
+			in_key.assign(optarg);
+			cout << "Encoding with key: " << in_key << endl;
+			break;
+		case '?':
+			if (optopt == 'e' || optopt == 'd' || optopt == 'k')
+				fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+			else if (isprint(optopt))
+				fprintf (stderr, "Unknown option '-%c'.\n", optopt);
+			else
+				fprintf (stderr, "Unknown option character '%d'.\n",	optopt);
+			return 1;
+		default:
+			abort();
+		}
+
+	if (optind != argc) {
+		cerr << "Non-option arguments are present." << endl;
 		return 1;
 	}
 
-	if (vigenere::read_file(file_name.c_str(), raw_buf)) {
+	if (vigenere::read_file(filename, raw_buf)) {
 		cerr << "Reading failed, make sure file exists and not empty. Terminate." << endl;
 		return 1;
 	}

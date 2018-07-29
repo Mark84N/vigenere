6e95158a588478843d4a0d182eb9a271288519c3
dieiev.dmitry <dieiev.dima@gmail.com>
2018-07-29 22:49:11 +0300

    added test script

---
+++ run_test.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+INPUT=input/*
+
+FILENUM=$(ls -1 input/ | wc -l)
+TESTDIR="$(pwd)/testdir"
+KEYS=("OPEN" "SAMSUNG" "INTELLIGENCE")
+
+mkdir $TESTDIR
+
+for src in $INPUT; do
+	filename=$(basename $src)
+	for key in ${KEYS[@]}; do
+		OUT="$TESTDIR/$(basename $src)_${key}"
+
+		printf "File \"%s\": word count %d\n" $filename $(cat $src | wc -w)
+		printf "Encrypting with \"%s\" ...\n" $key
+
+		./vigenere -e $src -k $key > $OUT
+
+		echo "Encrypted"
+
+		KEY="$(./vigenere -d $OUT | sed -n 's/Key: //p')"
+
+		if [ $KEY == $key ]; then
+			status="Success"
+		else
+			status="Fail"
+		fi
+
+		printf "Decryption finished. Derived key: %s. Status: %s\n\n" $KEY $status
+
+	done
+
+done
+
+rm -rf testdir

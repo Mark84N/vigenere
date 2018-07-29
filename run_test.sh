7b32b12c1ae3f1218ea670320dd0c48728bb6b0a
dieiev.dmitry <dieiev.dima@gmail.com>
2018-07-29 23:42:11 +0300

    again, fix script

--- run_test.sh
+++ run_test.sh
@@ -5,14 +5,24 @@ INPUT="input/*"
 TESTDIR="$(pwd)/testdir"
 KEYS=("OPEN" "SAMSUNG" "INTELLIGENCE")
 
+if [ ! -f ./vigenere ]; then
+	echo "No executable."
+	exit 1
+fi
+
+set -e
+
 mkdir $TESTDIR
 
 for src in $INPUT; do
 	filename="$(basename $src)"
+	printf "____________________________________\n\n"
+	printf "File \"%s\": word count %d\n" $filename $(cat $src | wc -w)
+	printf "____________________________________\n"
+
 	for key in ${KEYS[@]}; do
 		OUT="$TESTDIR/${filename}_${key}"
 
-		printf "File \"%s\": word count %d\n" $filename $(cat $src | wc -w)
 		printf "Encrypting with \"%s\" ...\n" $key
 
 		./vigenere -e $src -k $key > $OUT
@@ -21,7 +31,7 @@ for src in $INPUT; do
 
 		KEY="$(./vigenere -d $OUT | sed -n 's/Key: //p')"
 
-		if [ $KEY == $key ]; then
+		if [ "$KEY" == "$key" ]; then
 			status="Success"
 		else
 			status="Fail"
@@ -32,4 +42,7 @@ for src in $INPUT; do
 done
 
 rm -rf "$TESTDIR"
+
+set +e
+
 
